b5ea54a6e34edf7045c08e2a33ddfdf5c0bf8495
asanchezgiraldo <alejandro.sanchezgiraldo@vodafone.com.au>
2020-08-28 11:11:24 +1000

    I am a cowboy pushing to master and made my bot smarter -> sorry

--- app/index.js
+++ app/index.js
@@ -4,10 +4,13 @@ const cors = require('cors')
 
 let port = process.env.PORT || 3000;
 
-const data = require('./slackMardown.json');
+const data = require('./data.json');
+const slackMardown = require('./slackMardown.json');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.raw());
 app.use(cors())
 
 app.get('/', function ( req, res ) {
@@ -19,7 +22,23 @@ app.get('/health', function ( req, res ) {
 })
 
 app.post('/', function ( req, res ) {
-    res.send(data)
+    
+    switch(req.body.text) {
+        case "SIMO" || 'simo':
+            console.log('Go and get me some SIMO data mate');
+            res.send(data.SIMO);
+            break;
+        case 'HANDSET' || 'handset':
+            console.log('Go and get me some HANDSET data mate');
+            res.send(data.HANDSET);
+            break;
+        case 'USERNAME' || 'username':
+            console.log('Go and get me some USERNAME data mate');
+            res.send(data.USERNAME);
+            break;            
+        default:
+            res.send(slackMardown);
+      }
 })
 
 app.listen(port);
